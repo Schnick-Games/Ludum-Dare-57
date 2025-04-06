860c64ae4234cbaf577fadeb378721b2b9928ce5
PhilipSBeck <psbeck+1@gmail.com>
2025-04-06 21:58:01 +0100

    Boss level added and boss can be beaten

--- scripts/enemies/boss/boss.gd
+++ scripts/enemies/boss/boss.gd
@@ -1,11 +1,18 @@
 class_name Boss
 extends Area2D
 
+const WIN_SCREEN: Resource = preload("res://scenes/levels/win_screen.tscn")
+
 var sprite: AnimatedSprite2D
 
 var shaking: bool = false
-
 var time: float = 0
+
+var health: int = 1
+
+var attack_count:int = 0
+var attacks_until_weak:int = 5
+var is_weak: bool = false
 
 func start_battle():
 	$Timer.start()
@@ -22,20 +29,32 @@ func _process(delta: float) -> void:
 		sprite.position.x = 20 * (cos(time * 17) + cos(time * 7))
 
 func die():
-	#Show victory screen
-	pass
+	var win_screen = WIN_SCREEN.instantiate()
+	get_tree().root.add_child(win_screen)
 
 func attack(floor: int, left_side: bool):
+	attack_count += 1
 	sprite.frame = floor
 	sprite.flip_h = left_side
 	shaking = true
 	$ShakeTimer.start()
 
-
 func _on_timer_timeout() -> void:
-	attack(randi_range(0, 2), randi_range(0, 1))
-
+	if attack_count >= attacks_until_weak:
+		is_weak = true
+		attack_count = 0
+		$Timer.start(5)
+		sprite.frame = 3
+	else:
+		is_weak = false
+		attack(randi_range(0, 2), randi_range(0, 1))
 
 func _on_shake_timer_timeout() -> void:
 	shaking = false
 	sprite.position = Vector2(0,0)
+
+func damage_boss():
+	if is_weak:
+		health -= 1
+		if health <= 0:
+			die()
